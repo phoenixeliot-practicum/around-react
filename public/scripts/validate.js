7bb785a005a67e3bbc8e35ce182e9e51e851f5b6
Andrey Strogov <asthmaisback@gmail.com>
2020-05-07 03:03:34 +0300

    feat: add validate.js, ref index.html && index.js

---
+++ public/scripts/validate.js
@@ -0,0 +1,78 @@
+const showInputError = (formElement, inputElement, errorMessage, {errorClass, inputErrorClass}) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add(inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(errorClass);
+};
+
+const hideInputError = (formElement, inputElement, {errorClass, inputErrorClass}) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(inputErrorClass);
+  errorElement.classList.remove(errorClass);
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement, enums) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage, enums);
+  } else {
+    hideInputError(formElement, inputElement, enums);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement, inactiveButtonClass) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(inactiveButtonClass);
+  } else {
+    buttonElement.classList.remove(inactiveButtonClass);
+  }
+};
+
+const setEventListeners = (formElement, {inputSelector, submitButtonSelector, inactiveButtonClass, ...rest}) => {
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+  const buttonElement = formElement.querySelector(submitButtonSelector);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(formElement, inputElement, rest);
+      toggleButtonState(inputList, buttonElement, inactiveButtonClass);
+    });
+  });
+};
+
+const enableValidation = ({formSelector, ...rest}) => {
+  const getFormList = Array.from(document.querySelectorAll(formSelector));
+  getFormList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement, rest);
+  })
+};
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+});
+
+
+/* Саммари:
+* Код validate.js почти полностью есть в тренажере.
+* Поэтому вариант решения выше -- типичный пример и при проверке важно к нему стремиться.
+*
+* Студенты знают про rest/spread. Весь материал из файла validate.js есть в тренажере.
+*
+* Ключевой момент -- подвести к такой реализации тех, кто закликивает тренажер.
+* Не стесняйтесь отправлять студентов в тренажер.
+*
+* */
