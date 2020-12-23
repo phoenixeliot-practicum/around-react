4010057536a66f793fd28c1c86c14d9f2e3178db
Phoenix Eliot <phoenixjeliot@gmail.com>
2020-12-23 15:00:01 -0700

    Add class skeletons

---
+++ public/scripts/Popup.js
@@ -0,0 +1,7 @@
+export default class Popup {
+  constructor(popupSelector) {}
+  open() {}
+  close() {}
+  _handleEscClose() {}
+  setEventListeners() {}
+}
---
+++ public/scripts/UserInfo.js
@@ -0,0 +1,5 @@
+export default class UserInfo {
+  constructor(selectors) {}
+  getUserInfo() {}
+  setUserInfo() {}
+}
