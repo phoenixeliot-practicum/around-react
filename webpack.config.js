d29a0bb4133a9bc6e163eef64559fc7c324c7af9
Phoenix Eliot <phoenixjeliot+practicum@gmail.com>
2020-12-24 14:51:21 -0700

    Remove yarn, fix deploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,52 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: { main: "./src/index.js" },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-    publicPath: "/",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        exclude: "/node_modules/",
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
+module.exports = (env) => {
+  const config = {
+    entry: { main: "./src/index.js" },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "main.js",
+      publicPath: env.production ? "/around-react/" : "/",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          exclude: "/node_modules/",
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+              },
             },
-          },
-          "postcss-loader",
-        ],
-      },
-      {
-        test: /\.html$/,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|gif|woff|woff2)$/,
-        loader: "file-loader",
-      },
+            "postcss-loader",
+          ],
+        },
+        {
+          test: /\.html$/,
+          loader: "html-loader",
+        },
+        {
+          test: /\.(png|svg|jpg|gif|woff|woff2)$/,
+          loader: "file-loader",
+        },
+      ],
+    },
+    stats: "verbose",
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "src/index.html",
+      }),
+      new MiniCssExtractPlugin(),
     ],
-  },
-  stats: "verbose",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-    new MiniCssExtractPlugin(),
-  ],
+  };
+  console.log(config);
+  return config;
 };
